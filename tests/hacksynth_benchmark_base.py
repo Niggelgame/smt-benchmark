54197eadce02d7468a68146f4c160b5bf1dadbc7
Nicolas Edelmann <niggelgame@gmail.com>
2024-07-30 11:24:56 +0200

    add base framwork for hacksynth tests

--- tests/hacksynth_benchmark_base.py
+++ tests/hacksynth_benchmark_base.py
@@ -57,7 +57,22 @@ class HackSynthBenchmark_Base(TestBase):
         
         return repo_path
         
+    def get_test_cases(self, repo_path):
+            python = utils.get_own_python_executable()
 
+            # get all test cases
+            script = f"""cd {repo_path} && {python} {self.get_test_runner()} -T"""
+            self.log(f"Running command: {script}", 2)
+            try:
+                p = subprocess.run(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            except Exception as e:
+                self.log(f"Error: {e}", 1)
+                results = "error"
+                return
+            output = p.stdout.decode('utf-8')
+            # split output by comma for each test case as specified by "-T"
+            test_cases = output.strip().split(",")
+            return test_cases
 
 
     def run_test(self):
@@ -67,29 +82,34 @@ class HackSynthBenchmark_Base(TestBase):
         
         # run the tests
         try:
-            # get own python executable
+            # get own python executable which includes z3
             python = utils.get_own_python_executable()
+
+            test_cases = self.get_test_cases(path)
             
             # run the tests
-            with utils.timer() as elapsed:
-                script = f"""cd {path} && timeout -s SIGKILL {utils.get_test_timeout()} {python} {self.get_test_runner()} {' '.join(self.get_params())}"""
-                self.log(f"Running command: {script}", 2)
-                try:
-                    p = subprocess.run(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                except Exception as e:
-                    self.log(f"Error: {e}", 1)
-                    results = "error"
-                    return
-                output = p.stdout.decode('utf-8')
+            for test_case in test_cases:
+                self.log(f"Running test: {test_case}", 1)
+                with utils.timer() as elapsed:
+                    script = f"""cd {path} && timeout -s SIGKILL {utils.get_test_timeout()} {python} -t {test_case} {self.get_test_runner()} {' '.join(self.get_params())}"""
+                    self.log(f"Running command: {script}", 2)
+                    try:
+                        p = subprocess.run(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    except Exception as e:
+                        self.log(f"Error: {e}", 1)
+                        results[test_case] = "error"
+                        return
+                    output = p.stdout.decode('utf-8')
 
-                # timeout code predefined by the system
-                if utils.is_returncode_timeout(p.returncode):
-                    self.log(f"timed out", 1)
-                    results = "timeout"
-                else:
-                    self.log(f"Output: {output}", 3)
-                    results = elapsed()
-
+                    # timeout code predefined by the system
+                    if utils.is_returncode_timeout(p.returncode):
+                        self.log(f"timed out", 1)
+                        results[test_case] = "timeout"
+                    else:
+                        self.log(f"Output: {output}", 3)
+                        results[test_case] = elapsed()
+            
+            return results
         finally:
             # remove the temp directory
             os.system(f'rm -rf "{path}"')
