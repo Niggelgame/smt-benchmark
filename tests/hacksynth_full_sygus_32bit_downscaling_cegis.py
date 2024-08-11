d4c743ea764f7028bd49547e79e34242e9496ddd
Nicolas Edelmann <niggelgame@gmail.com>
2024-08-11 22:55:21 +0200

    add downscaling 32 bit sygus test

---
+++ tests/hacksynth_full_sygus_32bit_downscaling_cegis.py
@@ -0,0 +1,18 @@
+from tests.hacksynth_benchmark_base import HackSynthBenchmark_Base
+
+class HacksynthHackdelFullSygus(HackSynthBenchmark_Base):
+    def get_info(self):
+        base = super().get_info()
+        base["name"] = "HacksynthHackdelFullSygus"
+        base["description"] = "hacksynth benchmark, test cases are SyGuS benchmarks"
+        return base
+    
+    def get_test_runner(self):
+        return 'from_sygus_spec_hackdel_32bit.py'
+    
+
+    def get_params(self):
+        return ['-c SET', "-s synth_constants_cegis_stage"]
+    
+def create_test():
+    return HacksynthHackdelFullSygus()
