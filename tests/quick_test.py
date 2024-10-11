1f145cc9f01830073ce1bcfe46c6680b7fda4153
Nicolas Edelmann <niggelgame@gmail.com>
2024-10-10 18:05:09 -0700

    push quick test

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -3,14 +3,17 @@ from tests.hacksynth_benchmark_base import HackSynthBenchmark_Base
 class HacksynthStdHackdelBenchmark(HackSynthBenchmark_Base):
     def get_info(self):
         base = super().get_info()
-        base["name"] = "HacksynthStdHackdelBenchmarkHard"
+        base["name"] = "HacksynthStdHackdelBenchmarkBrahmaExact"
         return base
+    
+    def get_params(self):
+        return super().get_params() + ["-s synth_brahma -x"]
     
     def get_test_runner(self):
         return 'bitvec_benchmarks/hackdel.py'
     
-    def get_params(self):
-        return super().get_params() + ["-y 40 -c FREE"]
+    def get_test_cases(self, repo_path):
+        return ['p03', 'p24']
     
 def create_test():
     return HacksynthStdHackdelBenchmark()
